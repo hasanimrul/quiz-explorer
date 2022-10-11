82a232c4ca84ecb31c719707a3e70bd54bcef343
hasanimrul <mohammadhasan.imrul@gmail.com>
2022-10-12 01:08:45 +0600

    recharts install successfully

--- src/components/Quize/Quize.js
+++ src/components/Quize/Quize.js
@@ -5,9 +5,10 @@ import './Quize.css';
 
 const Quize = () => {
   const course = useLoaderData().data;
-  const { questions } = course;
+  const { questions, name } = course;
   return (
     <div className="quize-container">
+      <h2>Quiz of {name}</h2>
       {questions.map((question) => (
         <QuizeQuestion key={question.id} questions={question}></QuizeQuestion>
       ))}
