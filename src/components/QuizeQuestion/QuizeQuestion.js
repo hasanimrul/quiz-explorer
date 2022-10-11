6a538e431b09d48273888916ad5b64702f6ff137
hasanimrul <mohammadhasan.imrul@gmail.com>
2022-10-12 01:44:48 +0600

    eye icon added and toast install successfully

--- src/components/QuizeQuestion/QuizeQuestion.js
+++ src/components/QuizeQuestion/QuizeQuestion.js
@@ -1,16 +1,30 @@
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { Link } from "react-router-dom";
 import AnswerOption from "../AnswerOption/AnswerOption";
 import './QuizeQuestion.css'
 
 const QuizeQuestion = ({ questions }) => {
-  const { question, options } = questions;
-  console.log(questions);
+  const { question, options, correctAnswer } = questions;
+//   console.log(correctAnswer);
+
+  const handleCorrectAnswer = () =>{
+    alert(correctAnswer);
+  }
   return (
     <div className="quize-question-container">
-      <h2>Ques: {question.slice(3,-4)}</h2>
-      {options.map((option) => (
+
+      <div className="question-div">
+            <h2>Ques: {question.slice(3,-4)}</h2>
+            <Link onClick={handleCorrectAnswer}><FontAwesomeIcon icon={faEye}></FontAwesomeIcon></Link>
+      </div>
+
+      {
+        options.map((option) => (
         <AnswerOption question={questions} option={option}></AnswerOption>
-      ))}
+        ))
+      }
     </div>
   );
 };
