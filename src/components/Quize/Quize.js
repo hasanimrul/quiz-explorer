f5be0ef3585d8198aa6a22148a0998cf64ab5be7
hasanimrul <mohammadhasan.imrul@gmail.com>
2022-10-12 01:26:32 +0600

    charts showing successfully in statistics page

--- src/components/Quize/Quize.js
+++ src/components/Quize/Quize.js
@@ -5,13 +5,16 @@ import './Quize.css';
 
 const Quize = () => {
   const course = useLoaderData().data;
+
   const { questions, name } = course;
   return (
     <div className="quize-container">
       <h2>Quiz of {name}</h2>
-      {questions.map((question) => (
+      {
+      questions.map((question) => (
         <QuizeQuestion key={question.id} questions={question}></QuizeQuestion>
-      ))}
+      ))
+      }
     </div>
   );
 };
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { useLoaderData } from 'react-router-dom';
+import './Statistics.css';
 
 const Statistics = () => {
+    const statistics = useLoaderData().data;
     return (
-        <div>
-            
+        <div className='statistics'>
+            <h2>Statistics of Quiz</h2>
+            <LineChart width={600} height={300} data={statistics} margin={{ top: 5, right: 20, 
+                bottom: 5, left: 5 }}>
+                <Line type="monotone" dataKey="total" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+            </LineChart>
         </div>
     );
 };
