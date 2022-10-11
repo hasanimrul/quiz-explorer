9f84fc07a790b2b8f3dc7d2a52952d5bab267628
hasanimrul <mohammadhasan.imrul@gmail.com>
2022-10-12 01:52:00 +0600

    toast added successfully for showing correct answer

--- src/components/QuizeQuestion/QuizeQuestion.js
+++ src/components/QuizeQuestion/QuizeQuestion.js
@@ -4,21 +4,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import AnswerOption from "../AnswerOption/AnswerOption";
 import './QuizeQuestion.css'
+  import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 const QuizeQuestion = ({ questions }) => {
   const { question, options, correctAnswer } = questions;
-//   console.log(correctAnswer);
 
-  const handleCorrectAnswer = () =>{
-    alert(correctAnswer);
-  }
+ 
+  const notify = () => toast(correctAnswer);
+  
   return (
     <div className="quize-question-container">
-
+        
       <div className="question-div">
             <h2>Ques: {question.slice(3,-4)}</h2>
-            <Link onClick={handleCorrectAnswer}><FontAwesomeIcon icon={faEye}></FontAwesomeIcon></Link>
+            <Link onClick={notify}><FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+            <ToastContainer />
+            </Link>
       </div>
+      
 
       {
         options.map((option) => (
